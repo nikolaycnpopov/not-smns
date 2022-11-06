45d53ff6bff658e88bf50f9bb575a7156a354799
nikolaycnpopov <nikolay.cn.popov@gmail.com>
2022-11-06 22:33:51 +0300

    Added talisman use ability to archangel and banshee. Formatting fix for changelog.md.

---
+++ units/leaders/leaders-insert.sql
@@ -0,0 +1,21 @@
+-- !!!!!!!!!!!!!
+-- Нужно выполнить этот скрипт на новом проекте ровно один раз!
+-- !!!!!!!!!!!!!
+
+-- Империя - Архангел
+insert into "C:\Users\nikol\not-smns-project\NOT sMNS v1.0.1.2\Globals\GLabi.dbf" (unit_id, l_ability) values ('g000uu0022', 15)
+
+-- Проверка
+-- select * from "C:\Users\nikol\not-smns-project\NOT sMNS v1.0.1.2\Globals\GLabi.dbf" where unit_id = 'g000uu0022'
+
+-- Откат
+-- delete from "C:\Users\nikol\not-smns-project\NOT sMNS v1.0.1.2\Globals\GLabi.dbf" where unit_id = 'g000uu0022' and l_ability = 15
+
+-- Орды Нежити - Баньши
+insert into "C:\Users\nikol\not-smns-project\NOT sMNS v1.0.1.2\Globals\GLabi.dbf" (unit_id, l_ability) values ('g000uu0099', 15)
+
+-- Проверка
+-- select * from "C:\Users\nikol\not-smns-project\NOT sMNS v1.0.1.2\Globals\GLabi.dbf" where unit_id = 'g000uu0099'
+
+-- Откат
+-- delete from "C:\Users\nikol\not-smns-project\NOT sMNS v1.0.1.2\Globals\GLabi.dbf" where unit_id = 'g000uu0099' and l_ability = 15
